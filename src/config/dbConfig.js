52e2b9369aabba275617b4bb306a84a134a72a7d
Francesco <goras.francesco@gmail.com>
2021-04-26 23:05:55 +1200

    Tidy up dbConfig. Update node-postgres (fixed connection bug!)

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -4,8 +4,11 @@ require('dotenv').config({ path: path.join(__dirname, '.env') });
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
-    ssl: false,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 module.exports = { pool };
