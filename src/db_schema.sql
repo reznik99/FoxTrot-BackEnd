25e04762696881d4c0e9b6d2c77d00cb52d153a8
Francesco <goras.francesco@gmail.com>
2021-05-24 22:54:36 +1200

    added rsa public key to users table. Edited Contacts table

--- src/db_schema.sql
+++ src/db_schema.sql
@@ -1,13 +1,13 @@
 
 CREATE TABLE users(
     phone_no varchar(16) NOT NULL,
+    public_key text,
     password CHAR(60) NOT NULL,
     id SERIAL NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE contacts(
-    user1 SERIAL NOT NULL REFERENCES users(id),
-    user2 SERIAL NOT NULL REFERENCES users(id),
-    nickname varchar(20) NOT NULL,
-    PRIMARY KEY(user1, user2)
+    id SERIAL NOT NULL PRIMARY KEY
+    user_id SERIAL NOT NULL REFERENCES users(id)
+    contact_id SERIAL NOT NULL REFERENCES users(id)
 );
