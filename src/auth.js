82bd38e3a1265ace5b9589cd46dbe636a0f5ad60
Francesco <goras.francesco@gmail.com>
2023-01-09 01:22:10 +1300

    Refactor auth code to use async/await and catch all errors to prevent request timeout

--- src/auth.js
+++ src/auth.js
@@ -10,12 +10,10 @@ const pool = require('./config/dbConfig').pool;
 const BCRYPT_SALT_ROUNDS = 12;
 
 module.exports = (passport) => {
-    passport.serializeUser(function (user, done) {
-        done(null, user.id);
-    });
+    passport.serializeUser((user, done) => done(null, user.id));
 
-    passport.deserializeUser(function (id, done) {
-        User.findById(id, function (err, user) {
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => {
             done(err, user);
         });
     });
@@ -25,79 +23,62 @@ module.exports = (passport) => {
         usernameField: 'phone_no',
         passwordField: 'password',
         session: false,
-    }, (phone_no, password, done) => {
+    }, async (phone_no, password, done) => {
+        try {
+            const existingRes = await pool.query('SELECT phone_no FROM users WHERE phone_no=$1', [phone_no])
+            if (existingRes.rows.length > 0)
+                return done(null, false, { message: 'Username already taken!' })
 
-        pool.query('SELECT phone_no FROM users WHERE phone_no=$1', [phone_no], (err, results) => {
-            if (err) {
-                console.error(err.stack)
-            } else {
-                if (results.rows.length > 0)
-                    return done(null, false, { message: 'Phone number already registered!' });
+            // Hash password and create new user
+            const hash = await bcrypt.hash(password, BCRYPT_SALT_ROUNDS)
+            const res = await pool.query('INSERT INTO users(phone_no, password) VALUES ($1, $2) RETURNING *', [phone_no, hash])
 
-                // Create new user (HASH PASSWORD)
-                bcrypt.hash(password, BCRYPT_SALT_ROUNDS, (err, hash) => {
-                    pool.query('INSERT INTO users(phone_no, password) VALUES ($1, $2) RETURNING *', [phone_no, hash], (err, results) => {
-                        // Catch both bcrypt and psql errors, log them and return generic internal error
-                        if (err) {
-                            console.error(err)
-                            return done(null, false, { message: 'Error occoured during registration. Please try again later.' });
-                        }
-                        return done(null, results.rows[0]);
-                    });
-                });
-            }
-        });
-    })
-    );
+            return done(null, res.rows[0]);
+
+        } catch (err) {
+            console.error("Signup err: ", err)
+            return done(null, false, { message: 'Error occoured during registration. Please try again later.' })
+        }
+    }));
 
     passport.use('login', new LocalStrategy({
         usernameField: 'phone_no',
         passwordField: 'password',
         session: false,
-    }, (phone_no, password, done) => {
+    }, async (phone_no, password, done) => {
+        try {
+            const results = await pool.query('SELECT * FROM users WHERE phone_no=$1', [phone_no]);
+            if (results.rows.length !== 1)
+                return done(null, false, { message: 'Invalid username and/or password' });
 
-        pool.query('SELECT * FROM users WHERE phone_no=$1', [phone_no], (err, results) => {
+            // Compare hashes
+            const user = results.rows[0];
+            const equal = await bcrypt.compare(password, user.password)
+            if (!equal) return done(null, false, { message: 'Invalid username and/or password' });
 
-            if (err) {
-                console.log(err.stack)
-            } else {
-                if (results.rows.length < 1)
-                    return done(null, false, { message: 'Username doesn\'t exist' });
-
-                // Compare hashes
-                let user = results.rows[0];
-
-                bcrypt.compare(password, user.password).then(response => {
-                    // Invalid credentials
-                    if (response != true) {
-                        return done(null, false, { message: 'Invalid username and/or password' });
-                    }
-                    // Valid credentials
-                    return done(null, user);
-                });
-            }
-        });
-    }
-    ));
+            return done(null, user);
+        } catch (err) {
+            console.error("Login err: ", err)
+            return done(null, false, { message: 'Error during login process. Try again later' })
+        }
+    }));
 
     const opts = {
         jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme('JWT'),
         secretOrKey: jwtConfig.secret,
     };
 
-    passport.use('jwt', new JWTstrategy(opts, (jwt_payload, done) => {
-        //does user actually exist? This might not be necessary because JWT falsification shouldn't be possible without secret.
-        pool.query('SELECT * FROM users WHERE id=$1', [jwt_payload.id], (err, results) => {
-            if (err) {
-                console.log(err.stack)
-            } else {
-                if (results.rows.length < 1)
-                    return done(null, false, { message: 'Invalid Token!' });
+    passport.use('jwt', new JWTstrategy(opts, async (jwt_payload, done) => {
+        try {
+            // Not sure if this check is necessary
+            const results = await pool.query('SELECT * FROM users WHERE id=$1', [jwt_payload.id])
+            if (results.rows.length < 1)
+                return done(null, false, { message: 'Invalid Token!' });
 
-                //Valid Token
-                return done(null, results.rows[0]);
-            }
-        });
-    }),
-    );
+            return done(null, results.rows[0]);
+        } catch (err) {
+            console.error("JWT Verify err: ", err)
+            return done(null, false, { message: 'Error during Token verification process. Try again later' })
+        }
+    }));
 };
