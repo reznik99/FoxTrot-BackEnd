faae53fdc4b0e74a37e061768f99b37f84311c99
Francesco <goras.francesco@gmail.com>
2023-01-09 01:12:27 +1300

    fcm_token db changes + refactor

--- src/migrations/db_schema.sql
+++ src/migrations/db_schema.sql
@@ -1,9 +1,10 @@
 
 CREATE TABLE users(
+    id SERIAL NOT NULL PRIMARY KEY,
     phone_no varchar(16) NOT NULL,
-    public_key text,
     password CHAR(60) NOT NULL,
-    id SERIAL NOT NULL PRIMARY KEY
+    public_key text,
+    fcm_token text
 );
 
 CREATE TABLE contacts(
@@ -16,7 +17,7 @@ CREATE TABLE messages(
     id SERIAL NOT NULL PRIMARY KEY,
     user_id int NOT NULL REFERENCES users(id),
     contact_id int NOT NULL REFERENCES users(id),
-    message text NOT NULL,
     sent_at timestamptz NOT NULL DEFAULT now(),
-    seen boolean DEFAULT FALSE
+    seen boolean DEFAULT FALSE,
+    message text NOT NULL
 );
