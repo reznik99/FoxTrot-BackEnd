7cff5dfb7e0df14d7be684db75792dc0a8cf1e37
Francesco <goras.francesco@gmail.com>
2023-06-11 23:00:05 +1200

    Typescript migration part 1

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+
+import express from 'express'
+import passport from 'passport'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
+
+import { InitWebsocketServer } from "websockets"
+import { CreateRoutes } from "routes"
+import { InitAuth } from "auth"
+
+const PORT = parseInt(process.env.PORT || "1234")
+const app = express()
+
+// Middleware & Logging
+app.use(bodyParser.json({ limit: '10mb' }))
+app.use(morgan(':method :url :status :res[content-length] in :response-time ms'))
+app.use(passport.initialize())
+
+// Authentication & Routes
+InitAuth(passport)
+CreateRoutes(app, passport)
+
+// Start & Listen
+const expressServer = app.listen(PORT, () => {
+    console.info(`FoxTrot Server mode:${process.env.NODE_ENV} listening on ${PORT}`)
+})
+
+InitWebsocketServer(expressServer)
